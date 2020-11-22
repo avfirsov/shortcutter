58d23ff4c8af343486db859609a39fb76cbac89e
Andrew Firsov <afirsov410@gmail.com>
2020-11-22 17:46:00 +0300

    beginning of the article

--- shortcutter.ts
+++ shortcutter.ts
@@ -1,63 +1,53 @@
 import { combineLatest, fromEvent, Observable, Subscription, merge } from 'rxjs';
-import { filter, map, mapTo, pairwise, tap } from 'rxjs/operators';
+import { distinctUntilChanged, filter, map, mapTo, pairwise, tap } from 'rxjs/operators';
+
+const shortcuts = [
+  {
+    keys: ['Shift', 'r', 'x'], //something, that will be displayed in e.key
+    cb: () => {
+      //callback to call when keys are pressed
+      console.log('RxJS is cool!');
+    },
+  },
+  {
+    keys: ['Shift', 't', 's'],
+    cb: () => {
+      console.log('TS is awesome!');
+    },
+  },
+];
+
+const keydown$ = fromEvent(document, 'keydown');
+const keyup$ = fromEvent(document, 'keyup');
+
+// const keydown$ForKey = (key: string) =>
+//   //convert all to lower case because when shift is holded letter becomes uppercase in e.key
+//   keydown$.pipe(filter((e: KeyboardEvent) => e.key.toLowerCase() === key.toLowerCase()));
+// const keyup$ForKey = (key: string) =>
+//   keyup$.pipe(filter((e: KeyboardEvent) => e.key.toLowerCase() === key.toLowerCase()));
+
+const specifyKeyEvent = (observable: Observable<KeyboardEvent>) => (key: string) =>
+  observable.pipe(filter((e: KeyboardEvent) => e.key.toLowerCase() === key.toLowerCase()));
+
+const [keydown$ForKey, keyup$ForKey] = [keydown$, keyup$].map(specifyKeyEvent);
+
+const keyState$ForKey = (key: string) =>
+  merge(keydown$ForKey(key).pipe(mapTo(true)), keyup$ForKey(key).pipe(mapTo(false)));
+
+const keysState$FromKeys = (keys: string[]) => combineLatest(keys.map(keyState$ForKey));
+
+// keysState$FromKeys(['Shift', 'r', 'x']).subscribe(console.log);
 
 type Shortcut = {
   keys: string[];
   cb: { (): void };
 };
 
-type Options = {
-  preventDefault: boolean;
-  preventSeries: boolean;
-};
-
-const defaultOptions = {
-  preventDefault: true,
-  preventSeries: false,
-};
-
-const keydown$ = fromEvent(document, 'keydown');
-const keyup$ = fromEvent(document, 'keyup');
-
-const specifyKeyEvent = (observable: Observable<KeyboardEvent>) => (key: string) =>
-  observable.pipe(
-    filter((e: KeyboardEvent) => e.key.toLowerCase() === key.toLowerCase()),
-    tap((e) => defaultOptions.preventDefault && e.preventDefault())
-  );
-
-const [keydown$ForKey, keyup$ForKey] = [keydown$, keyup$].map(specifyKeyEvent);
-
-export class Shortcutter {
-  private _shortcuts: (Shortcut & { subscrition: Subscription })[] = [];
-  private options: Options = { ...defaultOptions };
-
-  public on(keys: string[], cb: { (): void }) {
-    const subscrition = combineLatest(
-      keys.map((key) => merge(keydown$ForKey(key).pipe(mapTo(true)), keyup$ForKey(key).pipe(mapTo(false))))
-    )
+const hotkeys = (shortcuts: Shortcut[]) =>
+  shortcuts.forEach(({ keys, cb }) =>
+    keysState$FromKeys(keys)
       .pipe(
-        pairwise(),
-        //предотвращаем залипание
-        filter(([prevState, curState]) => !this.options.preventSeries || prevState.some((p, i) => p !== curState[i])),
-        map(([_, curState]) => curState),
         filter((keysState) => keysState.every(Boolean))
       )
-      .subscribe(cb);
-
-    this._shortcuts.push({
-      keys,
-      cb,
-      subscrition,
-    });
-
-    return () => {
-      const ind = this._shortcuts.findIndex(({ subscrition: cSub }) => cSub === subscrition);
-      this._shortcuts = [...this._shortcuts.slice(0, ind), ...this._shortcuts.slice(ind + 1, this._shortcuts.length)];
-      subscrition.unsubscribe();
-    };
-  }
-
-  constructor(options?: Options) {
-    this.options = { ...this.options, ...options };
-  }
-}
+      .subscribe(cb)
+  );
